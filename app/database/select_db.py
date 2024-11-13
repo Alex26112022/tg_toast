94eadfad36f88b518517332c41f01993c57c234e
Алексей Денисенко <AlexeyDenisenko2703@yandex.ru>
2024-11-13 17:57:45 +0300

    add select_db

---
+++ app/database/select_db.py
@@ -0,0 +1,19 @@
+import asyncio
+import random
+
+from sqlalchemy import select
+
+from models import async_session, Toast
+
+
+class SelectDb:
+    """ Делает выборку из БД. """
+
+    @staticmethod
+    async def select_toasts(theme_id: int):
+        """ Выбирает случайный тост по теме. """
+        async with async_session() as session:
+            query = select(Toast).filter_by(theme_id=theme_id)
+            result = await session.execute(query)
+            toasts = result.scalars().all()
+            return random.choice(toasts)
